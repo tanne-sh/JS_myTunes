3b900f591ebb1764c12a8d1144d84497e4cc06ba
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-07-27 18:17:09 +0300

    Update index.js
    add functions for bottons

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,24 @@ import { videoPlayerInit } from "./videoPlayer.js";
 
 const playerBtn = document.querySelectorAll(".player-btn");
 const playerBlock = document.querySelectorAll(".player-block");
+const temp = document.querySelector(".temp");
+
+const deactivationPlayer = () => {
+  temp.style.display = "none";
+  playerBtn.forEach((item) => {
+    item.classList.remove("active");
+  });
+  playerBlock.forEach((item) => {
+    item.classList.remove("active");
+  });
+};
 
 playerBtn.forEach((btn, i) => {
-  console.log(btn);
-  console.log(i);
-
-  btn.addEventListener("click", () => {});
+  btn.addEventListener("click", () => {
+    deactivationPlayer();
+    btn.classList.add("active");
+    playerBlock[i].classList.add("active");
+  });
 });
 
 musicPlayerInit();
