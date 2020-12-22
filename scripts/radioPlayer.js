86765ca37c06020bb16c722b569d8ce7b08ee628
Tatyana Shurinova <tatyana.shurinova@gmail.com>
2020-12-22 17:18:29 +0200

    Update radioPlayer.js
    Добавлены кнопка вкл/стоп в проигрователь.  Добавлен функционал выбора радиостанций и переключение между нимию

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,1 +1,51 @@
-export const radioPlayerInit = () => {};
+export const radioPlayerInit = () => {
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioHeader = document.querySelector('.radio-header');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+
+  const audio = new Audio();
+  audio.tipy = 'audio/aac';
+
+  radioStop.disabled = true;
+
+  const chageIconPlay = () => {
+      if(audio.paused){
+        radio.classList.remove('play');
+        radioStop.classList.add('fa-play');
+        radioStop.classList.remove('fa-stop');
+      }else{
+        radio.classList.add('play')
+        radioStop.classList.add('fa-stop');
+        radioStop.classList.remove('fa-play');
+      }
+  }
+
+
+
+  radioNavigation.addEventListener('change', event => {
+    const target = event.target;
+    const parrent = target.closest('.radio-item');
+
+    radioItem.forEach(item => item.classList.remove('select'));
+    parrent.classList.add('select');
+
+    radioStop.disabled = false;
+    audio.src = target.dataset.radioStantion;
+    audio.play();
+    chageIconPlay();
+
+  });
+
+  radioStop.addEventListener('click', () => {
+    if(audio.paused){
+      audio.play();
+    } else{
+      audio.pause();
+    }
+    chageIconPlay();
+  });
+
+};
